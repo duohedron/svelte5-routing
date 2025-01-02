1b5764c370fc0ef55b590a68bb7215768dd3b48c
Peter Palotas <peter.palotas@evotrex.eu>
2025-01-02 12:05:48 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import type { Route, RouterOptions } from '@dvcol/svelte-simple-router';
+    import { link, RouterContext, RouterView } from '@dvcol/svelte-simple-router';
+    import Home from './routes/Home.svelte';
+    import Test from './routes/Test.svelte';
+
+    export const routes: Route[] = [
+        {
+            name: 'home',
+            path: '/',
+            component: Home,
+        },
+        {
+            name: 'test',
+            path: '/test/:id',
+            component: Test,
+        },
+    ] as const;
+
+    const options: RouterOptions = {
+        routes,
+        force: true,
+        strict: true,
+        hash: true
+    } as const;
+</script>
+
+<RouterContext {options}>
+    <a use:link href="/">Home</a>
+    <a use:link href="/test/1">Test 1</a>
+    <a use:link href="/test/2">Test 2</a>
+    <RouterView></RouterView>
+</RouterContext>
---
+++ src/routes/Test.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { useRoute } from '@dvcol/svelte-simple-router/router';
+
+    const router = useRoute();
+    const params = router.location?.params;
+
+    const id = params?.id;
+</script>
+
+<h1>Test</h1>
+<p>{id}</p>
